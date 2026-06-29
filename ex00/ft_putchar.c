4ad593934518c174fa5f1443491668b51f7c5a13
Dounia Saidi <douniasaidi62@gmail.com>
2026-06-29 15:18:39 +0100

    Update ft_putchar

--- ex00/ft_putchar.c
+++ ex00/ft_putchar.c
@@ -5,13 +5,13 @@ void ft_putchar(char c)
     write(1, &c, 1);
 }
 
-int main()
+/*int main()
 {
     ft_putchar('b');
     ft_putchar('r');
     ft_putchar('e');
     ft_putchar('l');
     ft_putchar('\n');
-     ft_putchar(65);
+    ft_putchar(65);
     return 0;
-}
+}*/
